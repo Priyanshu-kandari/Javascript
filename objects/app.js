7fb11abfd7bcbd6ea669eff860bdba844a4f9c2f
Priyanshu-kandari <preyanshukandari@gmail.com>
2025-08-29 09:14:43 +0530

    objects

---
+++ objects/app.js
@@ -0,0 +1,8 @@
+// objects --> {label:data}
+const person = {
+    firstname: "huxn",
+    location:["planet","earth","khods"],
+    isprogrammer:true,
+}
+person.area ="dhram vihar";
+console.log(person)
