6bfe1a02a495f3f6842eee0704746a6f097b1758
Priyanshu-kandari <preyanshukandari@gmail.com>
2025-08-29 08:53:00 +0530

    arrays

---
+++ array/app.js
@@ -0,0 +1,60 @@
+//arrays -> 0 index based
+// const numbersarray=[1,2,3,4,5,6];
+// console.log(numbersarray);
+
+// const stringarr = ["eat", "sleep" , "code" , "repeat"];
+// console.log(stringarr);
+
+// //accessing item from array
+// console.log(stringarr[0]);
+
+//nested array
+// const nestarr = ["one", ["two", "three"], 1 , true , false];
+// console.log(nestarr);
+// console.log(nestarr[1] [1]);
+
+// 1. Create array name (favSingers) Store 3 favorite singers.
+// 2. log the first singer in that array.
+// 3. Create array name (fav Numbers) & store 4 fav Numbers.
+// 4. Create array name (mixedArr) store ["string", ["otherarray"], 123, true]
+// 5. Now Access each item in that array by using [] notation.
+
+// const favrappers = ["seedhemaut","calm","encore abj"];
+// console.log(favrappers[0]);
+// const favnumbers = ["1", "2", "3","4"]
+// const mixedArr = ["string", [favrappers], 123, true]
+// console.log(mixedArr[0])
+// console.log(mixedArr[1])
+// console.log(mixedArr[1] [0] [0]);
+// console.log(mixedArr[2])
+// console.log(mixedArr[3])
+
+
+//----------------------------------------------------------
+
+
+
+//basic array method
+
+// const fruits = ["apple","mango", "orange", "papaya"];
+// console.log(fruits)
+// fruits.push("kandari")
+// console.log(fruits)
+// fruits.pop()
+// console.log(fruits)
+// fruits.shift()
+// console.log(fruits)
+// fruits.unshift("priyanshu")
+// console.log(fruits)
+// const newfruits =["banana","pear","strawberry"]
+// const totalfruits = fruits.concat(newfruits)
+// console.log(totalfruits)
+
+// const pl = ["js","golang","python","php"]
+// const numbers =[1,5,5,78,76,32,645,6,45,657,5,67,7,8,9,1000]
+// console.log(pl.includes("golang"))
+// console.log(pl.join("-"))
+// console.log(pl.reverse())
+// console.log(pl.slice(0,4))
+// console.log(numbers)
+// console.log(numbers.sort())
