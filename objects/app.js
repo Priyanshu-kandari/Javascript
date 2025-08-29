37a65cb7559337513ac6d752bc7082c4ee71817f
Priyanshu-kandari <preyanshukandari@gmail.com>
2025-08-29 16:10:00 +0530

    objects completed

--- objects/app.js
+++ objects/app.js
@@ -1,8 +1,29 @@
 // objects --> {label:data}
-const person = {
-    firstname: "huxn",
-    location:["planet","earth","khods"],
-    isprogrammer:true,
-}
-person.area ="dhram vihar";
-console.log(person)
+// const person = {
+//     firstname: "huxn",
+//     location:["planet","earth","khods"],
+//     isprogrammer:true,
+// }
+// person.area ="dhram vihar";
+// person.school = "bdsvm";
+// delete person.firstname;
+// console.log(person)
+
+// 1. Create object name (car)
+// 2. add (type, model, color) as properties
+// 3. check the type of object
+// 4. Update the type property to "Toyota"
+// 5. Add new property wheels
+// 6. log car to the console.
+
+// const car = {
+//     type: "ford",
+//     model: "fortuner",
+//     color: "blue",
+// }
+
+// console.log(car.type)
+// car.type = "toyota";
+// console.log(car.type)
+// car.wheels ="alloy wheels"
+// console.log(car)
