a5495d92f1c22d781f2c167eb19d4d1ab1f3afbb
Priyanshu Kandari <preyanshukandari@gmail.com>
2025-09-07 19:34:58 +0530

    template strings completed

---
+++ intermediate/template string/app.js
@@ -0,0 +1,35 @@
+// console.log(`hello
+//     my name 
+//     is priyanshu
+//     kandari`);
+// console.log(`${2+2}`);
+
+// var firstname  = "Priyanshu"
+// console.log(`my name is ${firstname}`);
+
+// console.log(`${['one','two','three']}`);
+
+// function info(){
+//     return `my name is priyanshu kandari`
+// }
+
+// console.log(`${info()}`);
+
+// 1. Store this in template string ~
+// The quick
+// brown fox
+// jumps over
+// the lazy dog
+// 2. Create two variables (first Name, lastName)
+// 3. Use that variables in template string.
+
+
+// var sentence = `The quick
+// brown fox
+// jumps over
+// the lazy dog`
+// console.log(sentence);
+
+// const firstname = "Priyanshu";
+// const lastName = "Kandari";
+// console.log(`my name is ${firstname} ${lastName}`);
