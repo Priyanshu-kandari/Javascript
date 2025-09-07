7701d8c2cbea8839b7816e0ac8b1c4f2e6bf9455
Priyanshu Kandari <preyanshukandari@gmail.com>
2025-09-07 20:01:49 +0530

    arrow function completed

---
+++ intermediate/arrow function/app.js
@@ -0,0 +1,53 @@
+// function greet(username){
+//     return `hello ${username}`
+// }
+// console.log(greet("priyanshu"));
+
+// const greet = (username) => `hello ${username}`; console.log("hello");
+// console.log(greet("priyanshu"));
+
+// function double(x){
+//     return x*2;
+// }
+// console.log(double(5));
+
+// const double = (x)=>{
+//     return x*2
+// }
+// console.log(double(5));
+
+// const double = x =>x*2
+// console.log(double(5));
+
+//---------------------------------------------------------
+// setTimeout(function () {
+//     console.log("Hello");
+//     setTimeout(function () {
+//         console.log("Hey");
+//         setTimeout(function () {
+//             console.log("Namaste");
+//             setTimeout(function () {
+//                 console.log("Hi");
+//                 setTimeout(function () {
+//                     console.log("Bonjour");
+//                 }, 2000);
+//             }, 2000);
+//          }, 2000);
+//     }, 2000);
+// }, 2000);
+
+// setTimeout(() => {
+//     console.log("Hello");
+//     setTimeout(() => {
+//         console.log("Hey");
+//         setTimeout(() => {
+//             console.log("Namaste");
+//             setTimeout(() => {
+//                 console.log("Hi");
+//                 setTimeout(() => {
+//                     console.log("Bonjour");
+//                 }, 2000);
+//             }, 2000);
+//          }, 2000);
+//     }, 2000);
+// }, 2000);
