54c1872ddcd6549de2a9f52d2cfeae42ef97c0c7
Priyanshu Kandari <preyanshukandari@gmail.com>
2025-09-07 18:16:43 +0530

    small changes

--- beginner/date,time,setinterval&settimeout/app.js
+++ beginner/date,time,setinterval&settimeout/app.js
@@ -1,47 +1,46 @@
 // const date = new Date();
-// // console.log(date)
-// // const Year = date.getFullYear();
-// // const month = date.getMonth();
-// // const day = date.getDay();
-// // const hours = date.getHours();
-// // const minutes = date.getMinutes();
-// // const seconds = date.getSeconds();
-// // const milliseconds = date.getMilliseconds();
-// // console.log(`year ${Year}`)
-// // console.log(`month ${month}`)
-// // console.log(`day ${day}`)
-// // console.log(`hours ${hours}`)
-// // console.log(`minutes ${minutes}`)
-// // console.log(`seconds ${seconds}`)
-// // console.log(`milliseconds ${milliseconds}`)
+// console.log(date)
+// const Year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const milliseconds = date.getMilliseconds();
+// console.log(`year ${Year}`)
+// console.log(`month ${month}`)
+// console.log(`day ${day}`)
+// console.log(`hours ${hours}`)
+// console.log(`minutes ${minutes}`)
+// console.log(`seconds ${seconds}`)
+// console.log(`milliseconds ${milliseconds}`)
 
-// // console.log(date.toDateString());
-// // console.log(date);
-// // console.log(date.toISOString());
-// // console.log(date.toLocaleString());
-// date.setDate(date.getDate() - 1);
+// console.log(date.toDateString());
+// console.log(date);
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+// date.setDate(date.getDate() + 5);
 // console.log(date);
 
 
 //setinterval
 
-
-const intervalid = setInterval(()=>{
-    console.log("hello sir")
-}
-,1000)
+// const intervalid = setInterval(() => {
+//   console.log("priyanshu");
+// }, 2000);
 
 
-setTimeout(()=>{
-    console.log("this function will be executed after 5 seconds")
-},5000)
+
+// setTimeout(()=>{
+//     console.log("this function will be executed after 5 seconds")
+// },5000)
 
 
-//stop the interval after 10 seconds
+// //stop the interval after 10 seconds
 
-setTimeout(()=>{
-    clearInterval(intervalid)
-    console.log("interval stopped");
-},5000)
+// setTimeout(()=>{
+//     clearInterval(intervalid)
+//     console.log("interval stopped");
+// },5000)
 
-console.log(intervalid)
+// console.log(intervalid)
