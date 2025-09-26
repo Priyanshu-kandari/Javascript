fc8db1af6e851a96964f48f5a4419668d05b62df
Priyanshu <Preyanshukandari@gmail.com>
2025-09-26 19:41:01 +0530

    completed

---
+++ intermediate/Ternary operator/app.js
@@ -0,0 +1,6 @@
+// If the person have money they can "buy products"
+// If the person don't have any money "They should bring money"
+
+let doyouhavemoney = true
+let gavemoney = doyouhavemoney===true?"Buy products":"get a job"
+console.log(gavemoney);
